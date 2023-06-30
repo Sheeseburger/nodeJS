e55cfdc9ab8e215c71302e392915556c99d203e4
Mishka_Tishka <monyshchen@stud.khpi.ucode-connect.study>
2023-06-30 14:22:06 +0300

    added script for json-to-db converter

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+const dotenv = require('dotenv');
+dotenv.config({
+    path: `D:/course/4-natours/project/config.env`,
+});
+const mongoose = require('mongoose');
+
+const Tour = require('../../models/tourModel');
+
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB).then(() => {
+    console.log('DB connected!');
+});
+// read from file
+const tours = JSON.parse(
+    fs.readFileSync(
+        `${__dirname}/tours-simple.json`,
+        'utf-8'
+    )
+);
+
+//import data to db
+
+const importData = async () => {
+    try {
+        await Tour.create(tours);
+        console.log('Data successfully loaded!');
+        process.exit();
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+// Delete all data from DB
+
+const deleteData = async () => {
+    try {
+        await Tour.deleteMany();
+        console.log('deleted!');
+        process.exit();
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+if (process.argv[2] === '--import') {
+    importData();
+} else if (process.argv[2] == '--delete') {
+    deleteData();
+}
