444bbb01121780b8929038fd7975308a6eda03a0
Mishka_Tishka <monyshchen@stud.khpi.ucode-connect.study>
2023-07-05 11:28:36 +0300

    addded error class

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const morgan = require('morgan');
 
 const app = express();
 
+const AppError = require('./utils/appError');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+const globalErrorHandle = require('./controllers/errorController');
+
 //
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
@@ -14,22 +19,25 @@ app.use(express.json());
 
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-    console.log('Hello from the middleweare');
-    next();
-});
-
-app.use((req, res, next) => {
-    req.requestTime = new Date().toISOString();
-    next();
-});
+// app.use((req, res, next) => {
+//     req.requestTime = new Date().toISOString();
+//     next();
+// });
 
 // routes
 
-const tourRouter = require('./routes/tourRoutes');
-const userRouter = require('./routes/userRoutes');
-
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+    next(
+        new AppError(
+            `Can't find ${req.originalUrl} on this server :#`,
+            404
+        )
+    );
+});
+
+app.use(globalErrorHandle);
 
 module.exports = app;
