5b7c8ceaa3e969b01dce59d859155f177be4404e
Mishka_Tishka <monyshchen@stud.khpi.ucode-connect.study>
2023-08-07 15:29:36 +0300

    added updating user (by self)

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,6 +13,37 @@ exports.GetAllUsers = async (req, res) => {
         },
     });
 };
+
+const filterObj = (obj, ...alowedFields) => {
+    const newObj = {};
+    Object.keys(obj).forEach((el) => {
+        if (alowedFields.includes(el)) {
+            newObj[el] = obj[el];
+        }
+    });
+    return newObj;
+};
+
+// user updates be himself
+exports.updateMe = catchAsync(async (req, res, next) => {
+    // check if not updating password
+
+    if (req.body.password || req.body.confirmPassword)
+        return next(new AppError('This route is not for password updating'), 400);
+
+    // filter only allowed fields
+    const filteredBody = filterObj(req.body, 'name', 'email');
+    // update user data
+
+    const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, { new: true, runValidators: true });
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            user: updatedUser,
+        },
+    });
+});
 exports.createUser = (req, res) => {
     res.status(500).json({
         status: 'error',
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,29 +7,15 @@ const router = express.Router();
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 
-router.post(
-    '/forgotPassword',
-    authController.forgotPassword
-);
-router.patch(
-    '/resetPassword/:token',
-    authController.resetPassword
-);
-router.patch(
-    '/updateMyPassword',
-    authController.protect,
-    authController.updatePassword
-);
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
 
-router
-    .route('/')
-    .get(userController.GetAllUsers)
-    .post(userController.createUser);
+router.patch('/updateMe', authController.protect, userController.updateMe);
 
-router
-    .route('/:id')
-    .get(userController.getUser)
-    .patch(userController.updateUser)
-    .delete(userController.deleteUser);
+router.patch('/updateMyPassword', authController.protect, authController.updatePassword);
+
+router.route('/').get(userController.GetAllUsers).post(userController.createUser);
+
+router.route('/:id').get(userController.getUser).patch(userController.updateUser).delete(userController.deleteUser);
 
 module.exports = router;
