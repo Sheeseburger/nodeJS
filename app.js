a48ab3226f6a1959747bd81eeadcf72acfbb12ed
Sheeseburger <kekymber@gmail.com>
2023-10-14 13:14:46 +0300

    added limit for requests

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const rateLimit = require('express-rate-limit');
 const morgan = require('morgan');
 
 const app = express();
@@ -9,20 +9,24 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const globalErrorHandle = require('./controllers/errorController');
 
-//
+// GLOBAL Middleweare
+
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
-// Middleweare
-
+const limiter = rateLimit({
+    max: 10,
+    windowMs: 60 * 1000,
+    message: 'Too many requests from this IP, please try again later',
+});
 app.use(express.json());
-
+app.use('/api', limiter);
 app.use(express.static(`${__dirname}/public`));
 
-// app.use((req, res, next) => {
-//     req.requestTime = new Date().toISOString();
-//     next();
-// });
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    next();
+});
 
 // routes
 
@@ -30,12 +34,7 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-    next(
-        new AppError(
-            `Can't find ${req.originalUrl} on this server :#`,
-            404
-        )
-    );
+    next(new AppError(`Can't find ${req.originalUrl} on this server :#`, 404));
 });
 
 app.use(globalErrorHandle);
--- controllers/authController.js
+++ controllers/authController.js
@@ -14,11 +14,24 @@ const signToken = (id) => {
 };
 
 const createSendToken = (user, statucCode, res) => {
-    const token = signToken(user);
+    const token = signToken(user._id);
+    const cookieOptions = {
+        expires: new Date(
+            Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+        ),
 
+        httpOnly: true,
+    };
+    if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+    res.cookie('jwt', token, cookieOptions);
+    //remove password from oputput
+    user.password = undefined;
     res.status(statucCode).json({
         status: 'success',
         token,
+        data: {
+            user,
+        },
     });
 };
 
@@ -30,7 +43,7 @@ exports.signup = catchAsync(async (req, res, next) => {
         passwordConfirm: req.body.passwordConfirm,
         role: req.body.role,
     });
-    createSendToken(newUser._id, 201, res);
+    createSendToken(newUser, 201, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -45,7 +58,7 @@ exports.login = catchAsync(async (req, res, next) => {
     if (!user || !(await user.correctPassword(password, user.password))) {
         return next(new AppError('Incorrect email or password'), 401);
     }
-    createSendToken(user._id, 200, res);
+    createSendToken(user, 200, res);
 });
 
 exports.protect = catchAsync(async (req, res, next) => {
@@ -149,7 +162,7 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
     user.passwordResetExpires = undefined;
     await user.save();
     // update changedPasswordAt
-    createSendToken(user._id, 201, res);
+    createSendToken(user, 201, res);
 
     const token = signToken(user._id);
 
@@ -174,5 +187,5 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
     user.passwordConfirm = req.body.passwordConfirm;
     await user.save();
     // log user in, send JWT
-    createSendToken(user._id, 201, res);
+    createSendToken(user, 201, res);
 });
